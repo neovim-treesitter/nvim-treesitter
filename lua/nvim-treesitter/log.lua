6d0ca63e2106a10dd87ca1ac7766161e30a24aa1
George Harker <george@george-graphics.co.uk>
2026-04-13 09:53:39 -0700

    ensure logging is safe from async contexts

--- lua/nvim-treesitter/log.lua
+++ lua/nvim-treesitter/log.lua
@@ -54,7 +54,10 @@ end
 function Logger:info(m, ...)
   local m1 = m:format(...)
   messages[#messages + 1] = { 'info', self.ctx, m1 }
-  echo({ { mkpfx(self.ctx) .. ': ' .. m1, sev_to_hl.info } }, true, {})
+  local msg = { { mkpfx(self.ctx) .. ': ' .. m1, sev_to_hl.info } }
+  vim.schedule(function()
+    echo(msg, true, {})
+  end)
 end
 
 ---@param m string
@@ -62,7 +65,10 @@ end
 function Logger:warn(m, ...)
   local m1 = m:format(...)
   messages[#messages + 1] = { 'warn', self.ctx, m1 }
-  echo({ { mkpfx(self.ctx) .. ' warning: ' .. m1, sev_to_hl.warn } }, true, {})
+  local msg = { { mkpfx(self.ctx) .. ' warning: ' .. m1, sev_to_hl.warn } }
+  vim.schedule(function()
+    echo(msg, true, {})
+  end)
 end
 
 ---@param m string
@@ -71,7 +77,10 @@ end
 function Logger:error(m, ...)
   local m1 = m:format(...)
   messages[#messages + 1] = { 'error', self.ctx, m1 }
-  echo({ { mkpfx(self.ctx) .. ' error: ' .. m1, sev_to_hl.error } }, true, {})
+  local msg = { { mkpfx(self.ctx) .. ' error: ' .. m1, sev_to_hl.error } }
+  vim.schedule(function()
+    echo(msg, true, {})
+  end)
   return m1
 end
 
